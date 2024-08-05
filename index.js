e568d3d2dca0083e781534fee1ab6e2092cf5ee4
tabassumsabaa <sabihatabassum0511@gmail.com>
2024-08-05 08:29:47 +0530

    compleate booking data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,12 +26,114 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+   const hairCollection = client.db('fitZone').collection('hairservices');
+   const facialCollection = client.db('fitZone').collection('facialservices');
+   const yogaCollection = client.db('fitZone').collection('yogaServices');
+   const gymCollection = client.db('fitZone').collection('gymservices');
+   const bookingCollection = client.db('fitZone').collection('bookings');
+
+
+   // for hair
+   app.get('/hairservices', async(req, res) =>{
+       const cursor = hairCollection.find();
+       const result = await cursor.toArray();
+       res.send(result);
+   })
+
+   app.get('/hairservices/:id', async(req,res) =>{
+       const id = req.params.id;
+       const query = {_id: new ObjectId(id)};
+
+       const options = {
+        projection : {  title: 1, price: 1, service_id: 1, description: 1, img: 1}
+       }
+
+       const result = await hairCollection.findOne(query, options);
+       res.send(result);
+   })
+
+   //for facial
+   app.get('/facialservices', async(req, res) =>{
+       const cursor = facialCollection.find();
+       const result = await cursor.toArray();
+       res.send(result);
+   })
+
+   app.get('/facialservices/:id', async(req,res) =>{
+       const id = req.params.id;
+       const query = {_id: new ObjectId(id)};
+
+       const options = {
+        projection : {  title: 1, price: 1, servicef_id: 1, service_description: 1, img: 1}
+       }
+
+       const result = await facialCollection.findOne(query, options);
+       res.send(result);
+   })
+
+     //for yoga
+     app.get('/yogaServices', async(req, res) =>{
+        const cursor = yogaCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+ 
+    app.get('/yogaServices/:id', async(req,res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+ 
+        const options = {
+         projection : {  title: 1, price: 1, provide_id: 1, provide_description: 1, img:1}
+        }
+ 
+        const result = await yogaCollection.findOne(query, options);
+        res.send(result);
+    })
+
+        //for gym
+        app.get('/gymservices', async(req, res) =>{
+            const cursor = gymCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+     
+        app.get('/gymservices/:id', async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+     
+            const options = {
+             projection : {  title: 1, price: 1, provide_id: 1, provide_description: 1, img: 1 }
+            }
+     
+            const result = await gymCollection.findOne(query, options);
+            res.send(result);
+        })
+
+        // bookings
+
+        app.get('/bookings', async(req, res) =>{
+            console.log(req.query.email);
+            let query = {};
+            if (req.query?.email) {
+                query = {email: req.query.email}                
+            }
+            const result = await bookingCollection.find().toArray();
+            res.send(result);            
+        })
+        app.post("/bookings", async(req, res) =>{
+            const booking = req.body;
+            console.log(booking);
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   // await client.close();
   }
 }
 run().catch(console.dir);
